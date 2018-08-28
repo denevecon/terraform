c1322c507668d73891c2b513ec39b2f6a448832a
Coert de Neve <coert@de-neve.nl>
2018-08-28 22:13:50 +0200

    Provider separaat

---
+++ code/instance.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  access_key = "AKIAJBS5MEYZOW67VGDA"
+  secret_key = "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"
+  region     = "eu-west-1"
+}
--- code/main.tf
+++ code/main.tf
@@ -2,10 +2,6 @@ terraform {
   required_version = ">= 0.8"
 }
 
-provider "aws" {
-  region = "eu-west-1"
-}
-
 resource "aws_vpc" "example" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
