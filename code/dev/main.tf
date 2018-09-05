f9b72ef4c92b216a062467ea9fafc678da4f5487
Coert de Neve <coert@de-neve.nl>
2018-09-05 13:13:15 +0200

    Next

--- code/dev/main.tf
+++ code/dev/main.tf
@@ -1,10 +1,10 @@
 module "vpc" {
-  source = "modules/vpc/"
+  source = "../modules/vpc/"
 
   # name = "complete-example"
 
   cidr = "10.10.0.0/16"
-  azs = "eu-west-1a"
+  azs  = "eu-west-1a"
 
   # azs                 = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
   # private_subnets     = ["10.10.1.0/24", "10.10.2.0/24", "10.10.3.0/24"]
